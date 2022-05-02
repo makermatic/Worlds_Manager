f4c9366c288cf0fa84971cf27bc098204170bcce
makermatic <44180512+makermatic@users.noreply.github.com>
2022-05-02 19:50:41 -0400

    Simplified Matchup and Casters Panels

--- bundles/worlds_bundle/dashboard/talent.js
+++ bundles/worlds_bundle/dashboard/talent.js
@@ -1,42 +1,16 @@
 const leftCaster = document.getElementById('leftCaster')
 const rightCaster = document.getElementById('rightCaster')
 
-const analyst1 = document.getElementById('analyst1')
-const analyst2 = document.getElementById('analyst2')
-
-const leftOther = document.getElementById('leftCasterOther')
-const rightOther = document.getElementById('rightCasterOther')
-const analystOther1 = document.getElementById("analystOther1")
-const analystOther2 = document.getElementById("analystOther2")
-
 var swapTog = !true;
 
 function update() {
-    const data = {
-        leftCaster: leftCaster.value,
-        rightCaster: rightCaster.value,
-        analyst1: analyst1.value,
-        analyst2: analyst2.value,
-        leftOther: leftOther.value,
-        rightOther: rightOther.value,
-        analystOther1: analystOther1.value,
-        analystOther2: analystOther2.value
-    }
+    const data = { leftCaster: leftCaster.value, rightCaster: rightCaster.value }
     nodecg.sendMessage('updateCasters', data)
 }
 
 function swap() {
     if (swapTog != true) {
-        const data = {
-            leftCaster: leftCaster.value,
-            rightCaster: rightCaster.value,
-            analyst1: analyst1.value,
-            analyst2: analyst2.value,
-            leftOther: leftOther.value,
-            rightOther: rightOther.value,
-            analystOther1: analystOther1.value,
-            analystOther2: analystOther2.value
-        }
+        const data = { leftCaster: leftCaster.value, rightCaster: rightCaster.value }
         nodecg.sendMessage('swapCasters', data)
         swapTog = true;
     } else {
