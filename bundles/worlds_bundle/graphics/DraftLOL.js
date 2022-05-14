fad18a5102f789525a318d6d02d32667df1c88ff
makermatic <44180512+makermatic@users.noreply.github.com>
2022-05-14 13:02:41 -0400

    Removed unused methods (commented initially)

--- bundles/worlds_bundle/graphics/DraftLOL.js
+++ bundles/worlds_bundle/graphics/DraftLOL.js
@@ -7,13 +7,9 @@ const team2Name = document.getElementById('team2Name');
 nodecg.listenFor('updateTeams', (data) => {
     team1Name.innerHTML = data.team1Name
     team2Name.innerHTML = data.team2Name
-        // changeTeam1Image()
-        // changeTeam2Image()
 })
 
 nodecg.listenFor('swapTeams', (data) => {
     team2Name.innerHTML = data.team1Name
     team1Name.innerHTML = data.team2Name
-        // changeTeam1Image()
-        // changeTeam2Image()
 })
