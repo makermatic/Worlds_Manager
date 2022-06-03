d3a4db0e21a9958082148d292deea8ddbeae2c6c
makermatic <44180512+makermatic@users.noreply.github.com>
2022-06-03 18:02:58 -0400

    Added numbers to DraftLOL and fixed Score Overlay formatting

--- bundles/worlds_bundle/graphics/DraftLOL.js
+++ bundles/worlds_bundle/graphics/DraftLOL.js
@@ -7,6 +7,9 @@ const team2Name = document.getElementById('team2Name');
 const LCaster = document.getElementById('leftCaster');
 const RCaster = document.getElementById('rightCaster');
 
+const team1Rep = nodecg.Replicant('team1');
+const team2Rep = nodecg.Replicant('team2');
+
 
 nodecg.listenFor('updateTeams', (data) => {
     team1Name.innerHTML = data.team1Name
@@ -28,22 +31,36 @@ nodecg.listenFor('swapCasters', (data) => {
     LCaster.innerHTML = data.rightCaster
 })
 
+team1Rep.on('change', (newVal) => {
+    team1El.innerHTML = newVal;
+})
+
+team2Rep.on('change', (newVal) => {
+    team2El.innerHTML = newVal;
+})
+
 
 // Style Listeners
 nodecg.listenFor('IBS', () => {
     document.body.style.backgroundImage = 'url("layouts/IBS/Draftlol.png")';
+    team1.style.color = '#460000'
+    team2.style.color = '#460000'
     team1Name.style.color = '#460000'
     team2Name.style.color = '#460000'
 })
 
 nodecg.listenFor('Gold', () => {
     document.body.style.backgroundImage = 'url("layouts/Gold/Draftlol.png")';
+    team1.style.color = '#d99500'
+    team2.style.color = '#d99500'
     team1Name.style.color = '#d99500'
     team2Name.style.color = '#d99500'
 })
 
 nodecg.listenFor('Plat', () => {
     document.body.style.backgroundImage = 'url("layouts/Platinum/Draftlol.png")';
+    team1.style.color = '#256154'
+    team2.style.color = '#256154'
     team1Name.style.color = '#256154'
     team2Name.style.color = '#256154'
 })
